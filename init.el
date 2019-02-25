1a7291eb9714664db18b7107fcfa353fb1e84306
Kamil Ziemian <kziemianfvt@gmail.com>
2019-02-25 12:45:23 +0100

    Better init.el, 1

--- init.el
+++ init.el
@@ -1,6 +1,26 @@
 ;; Setting up build-in variables
-(setq inhibit-startup-message t)	; Disable start up message.
+(setq inhibit-startup-message t)  ; Disable start up message.
 (setq next-line-add-newlines t)
+(setq sentence-end-double-space-nil)
+
+
+;; Setting up predicators
+(fset 'yes-or-no-p 'y-or-n-p)   ; Change question from "yes or no" to "y or n"
+
+
+
+
+
+;; ##################
+;; Setting up backups management
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+(setq delete-old-version -1)
+(setq version-control t)
+(setq vc-make-backup-files t)
+(setq auto-save-file-name-transforms
+      '((".*" "~/.emacs.d/auto-save-list" t)))
+
+
 
 
 
@@ -8,10 +28,19 @@
 ;; Configuration of package menager
 (require 'package)
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
 
+;; Setting up list of repositories
+(setq package-archives
+      '("gnu" . "https://elspa.gnu.org/packages/"))
+(add-to-list 'package-arichves
+	     '("marmelade" . "https://marmalade-repo.org"))
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org"))
+
+;; 
 (package-initialize)
+
+
 
 
 
@@ -39,12 +68,16 @@
 
 
 
+
+
 ;; ##################
 ;; Installing packeges
+
 
 ;; #####
 (use-package try
   :ensure t)
+
 
 ;; #####
 (use-package which-key
