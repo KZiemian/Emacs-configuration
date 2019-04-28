7f8d424d600fabe9c164e248fa764a9373d9b7f2
Kamil Ziemian <kziemianfvt@gmail.com>
2019-04-28 15:41:52 +0200

    `configadd.org` added

--- init.el
+++ init.el
@@ -106,17 +106,3 @@
 
 (setq ibuffer-default-sorting-mode 'major-mode)  ; Sorts buffers
 ;; in buffers list by major mode of buffer
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (yasnippet-snippets ox-reveal htmlize jedi elpy yasnippet flycheck zenburn-theme which-key use-package undo-tree try swiper-helm rust-playground rust-mode rainbow-delimiters org-bullets lorem-ipsum latex-pretty-symbols helm-gtags function-args counsel color-theme-modern color-theme beacon badwolf-theme badger-theme auto-complete auctex-latexmk aggressive-indent achievements ace-window))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
